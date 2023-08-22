2a8894cb755d741126851fc1a520e9cd5b795bc6
Anna211291 <Anjutka777@gmail.com>
2023-08-22 00:27:27 -0400

    colors

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const elements = {
+  bodyColor: document.querySelector("body"),
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]')
+}
+let timerId = null;
+elements.btnStart.addEventListener("click", hendlerClickOn);
+elements.btnStop.addEventListener("click", hendlerClickOff);
+
+function hendlerClickOn() {
+
+    timerId = setInterval(() => {
+        elements.bodyColor.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+    elements.btnStart.disabled = true;
+    elements.btnStop.disabled = false;
+}
+function hendlerClickOff() {
+    clearInterval(timerId);
+    elements.bodyColor.style.backgroundColor = '#fff';
+elements.btnStart.disabled = false;
+elements.btnStop.disabled = true;
+}
